c546f824d987b7b22404c044e3303f2c8417cfa9
Stuvstad <stuvstak@oregonstate.edu>
2019-04-09 18:13:29 -0700

    more additions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //map created
 var planemap = L.map('map', {
-  center: [],
+  center: [39.828371, -98.579480],
   zoom: 5,
   maxZoom: 15,
   minZoom: 2,
@@ -8,3 +8,13 @@ var planemap = L.map('map', {
 });
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(planemap);
+
+//adds airport points
+var airports = null;
+airports = L.geoJson.ajax("assets/airports.geojson",{
+  attribution: 'Airport data credentials'
+});
+airports.addTo(planemap);
+
+//adding color
+//var colors = chroma.scale('').mode('').colors();
